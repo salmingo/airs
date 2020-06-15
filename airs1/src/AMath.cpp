f8dfe594d2fc2ecb0f83a7b234eef592c85109c7
salmingo <lxm@nao.cas.cn>
2020-06-16 07:49:47 +0800

    200616
    update LUdcmp

--- airs1/src/AMath.cpp
+++ airs1/src/AMath.cpp
@@ -14,7 +14,15 @@
 
 namespace AstroUtil {
 //////////////////////////////////////////////////////////////////////////////
-bool LSFitLinear(int m, int n, double *x, double *y, double *c) {
+AMath::AMath() {
+
+}
+
+AMath::~AMath() {
+
+}
+
+bool AMath::LSFitLinear(int m, int n, double *x, double *y, double *c) {
 	bool rslt(false);
 	// A*c=Y ==> c = A^-1*Y
 	double *A  = new double[n * n];
@@ -44,11 +52,11 @@ bool LSFitLinear(int m, int n, double *x, double *y, double *c) {
 		*Yptr = t;
 	}
 	// 拟合系数
-	if (LUdecmp(n, A, idx)) {
-		LUbksb(n, A, idx, Y);
-		memcpy(c, Y, sizeof(double) * n);
-		rslt = true;
-	}
+//	if (LUdcmp(n, A, idx)) {
+//		LUbksb(n, A, idx, Y);
+//		memcpy(c, Y, sizeof(double) * n);
+//		rslt = true;
+//	}
 
 	delete []A;
 	delete []Y;
@@ -56,145 +64,91 @@ bool LSFitLinear(int m, int n, double *x, double *y, double *c) {
 	return rslt;
 }
 
-bool LUdecmp(int n, double *a, int *idx) {
-	bool rslt(true);
-	int row, col, k, imax, np(1);
-	double EPS(1.0E-20);
-	double amax, asum, t;
-	double *aptr, *aptr1, *aptr2;
-	double *scale = new double[n];
+bool AMath::LUdcmp(int n, double *a) {
+	ludcmp_.Reset(n, a);
 
-	// 统计行归一比例因子
-	for (row = 0, aptr = a; row < n; ++row) {// 遍历: 行
-		amax = 0.0;
-		for (col = 0; col < n; ++col, ++aptr) {// 遍历: 列
-			if ((t = fabs(*aptr)) > amax) amax = t;
+	vector<int> &idx = ludcmp_.idx;
+	int &np = ludcmp_.np;
+	const double TINY = 1.0E-30;
+	vector<double> scale(n);
+	int i, j, k, imax;
+	double t, big, *ptr, *ptr1, *ptr2;
+
+	np = 0;
+	for (i = 0, ptr = a; i < n; ++i) {
+		big = 0.0;
+		for (j = 0; j < n; ++j, ++ptr) {
+			if ((t = fabs(*ptr)) > big) big = t;
 		}
-		if (amax < EPS) {// 奇异矩阵
-			delete []scale;
+		if (big < TINY) {
+			np = -1;
+			return false;	// 奇异矩阵
+		}
+		scale[i] = 1.0 / big;
+	}
+
+	for (j = 0, ptr = a; j < n; ++j, ptr += n) {
+		big = 0.0;
+		for (i = j, ptr1 = ptr + j; i < n; ++i, ptr1 += n) {
+			if ((t = scale[i] * fabs(*ptr1)) > big) {
+				big  = t;
+				imax = i;
+			}
+		}
+		if (j != imax) {
+			for (k = 0, ptr1 = a + imax * n, ptr2 = ptr; k < n; ++k, ++ptr1, ++ptr2) {
+				t     = *ptr1;
+				*ptr1 = *ptr2;
+				*ptr2 = t;
+			}
+			++np;
+			scale[imax] = scale[j];
+		}
+		idx[j] = imax;
+		if (ptr[j] < TINY) {
+			np = -1;
 			return false;
 		}
-		scale[row] = 1.0 / amax;
-	}
-
-	// LU分解
-	for (col = 0; col < n && rslt; ++col) {
-		for (row = 0, aptr = a; row < col; ++row, aptr += n) {
-			asum = aptr[col];
-			for (k = 0, aptr1 = aptr, aptr2 = a + col; k < row; ++k, ++aptr1, aptr2 += n) {
-				asum -= (*aptr1 * *aptr2);
-			}
-			aptr[col] = asum;
-		}
-
-		for (row = col, amax = 0.0, aptr = a; row < n; ++row, aptr += n) {
-			asum = aptr[col];
-			for (k = 0, aptr1 = aptr, aptr2 = a + col; k < row; ++k, ++aptr1, aptr2 += n) {
-				asum -= (*aptr1 * *aptr2);
-			}
-			aptr[col] = asum;
-
-			if ((t = scale[row] * fabs(asum)) >= amax) {
-				imax = row;
-				amax = t;
-			}
-		}
-
-		if (col != imax) {
-			for (k = 0, aptr1 = a + imax * n, aptr2 = a + col * n; k < n; ++k, ++aptr1, ++aptr2) {
-				t      = *aptr1;
-				*aptr1 = *aptr2;
-				*aptr2 = t;
-			}
-			np = -np;	// 行交换次数的奇偶性
-			scale[imax] = scale[col];
-		}
-		idx[col] = imax;
-
-		if (fabs(t = a[col * n + col]) < EPS) rslt = false;
-		else {
-			t = 1.0 / t;
-			for (row = col + 1, aptr = a + row * n + col; row < n; ++row, aptr += n) {
-				*aptr = *aptr * t;
+		for (i = j + 1, ptr1 = ptr + n; i < n; ++i, ptr1 += n) {
+			t = ptr1[j] /= ptr[j];	//??? /= => / ???
+			for (k = j + 1, ptr2 = ptr + k; k < n; ++k, ++ptr2) {
+				ptr1[k] -= t * *ptr2;
 			}
 		}
 	}
-	delete []scale;
 
-	if (rslt) {
-		printf ("LUdecmp result:\n");
-		for (row = 0, aptr = a; row < n; ++row) {
-			for (col = 0; col < n; ++col, ++aptr) {
-				printf ("%f  ", *aptr);
-			}
-			printf ("\n");
-		}
-		printf ("\n");
-	}
-
-	return rslt;
+	return np != 0;
 }
 
-bool LUbksb(int n, double *a, int *idx, double *b) {
-	bool rslt(true);
-	int row, col, k, ii(-1);
-	double EPS(1.0E-20);
-	double sum;
-	double *aptr, *aptr1, *bptr;
+bool AMath::LUsolve(double *b, double *x) {
 
-	for (row = 0, aptr = a; row < n; ++row, aptr += n) {
-		k      = idx[row];
-		sum    = b[k];
-		b[k]   = b[row];
-		if (ii != -1) {
-			for (col = ii, aptr1 = aptr + col, bptr = b + col; col < row; ++col, ++aptr1, ++bptr) {
-				sum -= (*aptr1 * *bptr);
-			}
-		}
-		else if (fabs(sum) > EPS) ii = row;
-		b[row] = sum;
-	}
-
-	for (row = n - 1, aptr = a + row * n; row >= 0 && rslt; --row, aptr -= n) {
-		rslt = fabs(aptr[row]) > EPS;
-		if (rslt) {
-			sum = b[row];
-			for (col = row + 1, aptr1 = aptr + col, bptr = b + col; col < n; ++col, ++aptr1, ++bptr) {
-				sum -= (*aptr1 * *bptr);
-			}
-			b[row] = sum / aptr[row];
-		}
-	}
-
-	return rslt;
+	return false;
 }
 
 // 计算逆矩阵
-bool MatrixInvert(int n, double *a) {
+bool AMath::MatrixInvert(int n, double *a) {
 	int n2 = n * n;
 	double *y = new double[n2];
-	int *idx  = new int[n];
 	double *yptr;
 	int i;
 	bool rslt(false);
 
 	bzero(y, sizeof(double) * n2);
 	for (i = 0, yptr = y; i < n; ++i, yptr += (n + 1)) *yptr = 1.0;
-	if (LUdecmp(n, a, idx)) {
+	if (LUdcmp(n, a)) {
 		for (i = 0, yptr = y; i < n; ++i, ++yptr) {
-			LUbksb(n, a, idx, yptr);
+//			LUsolve(n, a, idx, yptr);
 		}
 		memcpy(a, y, sizeof(double) * n2);
 		rslt = true;
 	}
 
 	delete []y;
-	delete []idx;
 	return rslt;
 }
 
 // 计算矩阵乘积
-void MatrixMultiply(int m, int p, int n, double *L, double *R, double *Y) {
+void AMath::MatrixMultiply(int m, int p, int n, double *L, double *R, double *Y) {
 	int i, j, k;
 	double sum;
 	double *Lptr, *Rptr, *Yptr;
